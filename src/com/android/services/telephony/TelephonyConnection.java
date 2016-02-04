1ff9190785d8fd9fe7182caa25bcc9687c5bafb3
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-02-04 01:34:43 -0600

    Revert "Telephony: hook up call transfer"
    This reverts commit cdab421ed5b099e43e7915ceb61fc6b0223308c7.

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -480,16 +480,6 @@ abstract class TelephonyConnection extends Connection {
         }
     }
 
-    @Override
-    public void onTransfer() {
-        Log.v(this, "onTransfer");
-        try {
-            getPhone().explicitCallTransfer();
-        } catch (CallStateException e) {
-            Log.e(this, e, "Fail to transfer call.");
-        }
-    }
-
     public void performHold() {
         Log.v(this, "performHold");
         // TODO: Can dialing calls be put on hold as well since they take up the
@@ -607,11 +597,6 @@ abstract class TelephonyConnection extends Connection {
         if (phone != null && phone.isInEcm()) {
             callCapabilities |= CAPABILITY_SHOW_CALLBACK_NUMBER;
         }
-
-        if (phone != null && phone.canTransfer()) {
-            callCapabilities |= CAPABILITY_SUPPORTS_TRANSFER;
-        }
-
         return callCapabilities;
     }
 
